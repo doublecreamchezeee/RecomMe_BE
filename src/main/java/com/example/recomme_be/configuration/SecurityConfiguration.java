ef49c693c747b3378757ea75b93512baee364774
21120575 <nguyen.thanhtri2112@gmail.com>
2024-12-04 17:48:15 +0700

    fix: change firebase credentials

--- src/main/java/com/example/recomme_be/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/recomme_be/configuration/SecurityConfiguration.java
@@ -55,9 +55,10 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    private CorsConfigurationSource corsConfigurationSource() {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
         final var corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("https://recomme-five.vercel.app", "http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(List.of("https://recomme-five.vercel.app", "http://localhost:3000", "http://localhost:3003"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Origin", "Content-Type", "Accept"));
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
